dc563bb2dd8c57c378a75f92f86bcfecd79b10d7
Williams C <wilcontrerasf@gmail.com>
2024-03-18 21:48:21 -0300

    Inicio 09-useContext
    * se comienza a ver el uso de navegacion entre paginas para finalizar viendo el paso de info entre estas
    * se instala react-router-dom

---
+++ src/09-useContext/MainApp.jsx
@@ -0,0 +1,24 @@
+import {Navigate, Route, Routes} from 'react-router-dom'
+import { HomePage } from './HomePage'
+import { LoginPage } from './LoginPage'
+import { AboutPage } from './AboutPage'
+import { Navbar } from './Navbar'
+
+export const MainApp = () => {
+    return (
+        <>
+            <h1>MainApp</h1>
+
+            <Navbar />
+
+            <hr />
+
+            <Routes>
+                <Route path='/' element={<HomePage />}/>
+                <Route path='about' element={<AboutPage />}/>
+                <Route path='login' element={<LoginPage />}/>
+                <Route path='/*' element={<Navigate to="/about" />} />
+            </Routes>
+        </>
+    )
+}
